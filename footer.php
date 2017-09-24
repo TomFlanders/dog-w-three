4ce40fcf6bfa1a316b29bbbe5802b8d72e0f8f5c
Tom Flanders <tom@tomflanders.com>
2017-09-23 17:10:46 -0700

    Corrected copyright

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="w3-container w3-small w3-row w3-center dog-w-three-pad5top">
-Copyright 2016 Tom Flanders - Most rights reserved
+<div class="w3-container w3-small w3-row w3-center pad5top">
+Copyright <?php echo date("Y"); ?> Tom Flanders - Most rights reserved
 </div>
 <?php wp_footer(); ?>
