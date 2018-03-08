843dea30193c4b172c08e52870c42665f6e5f08b
Tom Flanders <tom@tomflanders.com>
2018-03-08 15:40:02 -0800

    Corrected feature image spacing

--- page.php
+++ page.php
@@ -20,12 +20,12 @@
 			while ( have_posts() ) : the_post();
 $format = get_post_format() ? : 'standard';
 ?>
-<article class="w3-container">
+<article class="w3-container" style="margin: 5px 0px 5px 0px;">
 <div id="post-><?php the_ID(); ?>" <?php post_class(); ?>>
   <header class="w3-container w3-teal w3-round w3-text-shadow w3-large">
   <span class="entry-title"><?php the_title(); ?></span>
   </header>
-
+<p></p>
 <div class="post-featured-image"><?php the_post_thumbnail(); ?></div>
 <?php the_content(); ?>
 
