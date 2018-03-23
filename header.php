26ef1a7cf32c9b9a9000cd3163531089a9722d4f
Tom Flanders <tom@tomflanders.com>
2018-03-22 18:37:52 -0700

    HTML fixes

--- header.php
+++ header.php
@@ -3,20 +3,10 @@
 
 <head>
 
-<?php
-if ( ! function_exists( '_wp_render_title_tag' ) ) {
-	function theme_slug_render_title() {
-?>
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<?php
-	}
-	add_action( 'wp_head', 'theme_slug_render_title' );
-}
-?>
-
+<?php if ( ! function_exists( '_wp_render_title_tag' ) ) { ?><title><?php wp_title( ); ?></title><?php } ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="/wp-content/themes/dog-w-three/w3.css">
-<link rel="stylesheet" type="text/css" href="/wp-content/themes/dog-w-three/style.css">
+<link rel="stylesheet" href="/wp-content/themes/dog-w-three/w3.css">
+<link rel="stylesheet" href="/wp-content/themes/dog-w-three/style.css">
 
 <meta charset="UTF-8">
 <?php wp_head(); ?>
@@ -26,16 +16,16 @@ if ( ! function_exists( '_wp_render_title_tag' ) ) {
 <body <?php body_class(); ?>>
 
 <!-- title bar -->
-  <div class="w3-teal w3-text-shadow w3-row dog-w-three-borderdouble">
-<table cellspacing=0 cellpadding=0>
+  <div class="w3-teal w3-text-shadow w3-row" style="border: double;">
+<table>
 <tr>
-<td class="w3-teal w3-third" height="100px">
+<td class="w3-teal w3-third">
   <?php the_custom_logo();?>
 </td>
-<td class="w3-teal w3-center w3-third" height="100px">  <h1><a class="dog-w-three-nouline" href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo('name'); ?></a></h1>
+<td class="w3-teal w3-center w3-third">  <h1><a style="text-decoration:none;" href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo('name'); ?></a></h1>
 <?php bloginfo('description'); ?>
 </td>
-<td class="w3-teal w3-third" height="100px">
+<td class="w3-teal w3-third">
 &nbsp;
 </td>
 </tr>
@@ -43,7 +33,7 @@ if ( ! function_exists( '_wp_render_title_tag' ) ) {
 </div>
 
 <!-- menu -->
-<div id="navmenu" class="dog-w-three-menu">
+<div id="navmenu" class="dogmenu">
   <?php wp_nav_menu( array( 'theme_location' => 'header-menu') );
   ?>
   </div>
